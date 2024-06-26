5d42abd58e0a7d9fcfbb55dc33a73ca4f9862ecf
Basith5075 <65912815+Basith5075@users.noreply.github.com>
2024-06-25 22:37:38 -0500

    Splunk integration

--- campaign/src/main/java/com/campaign/service/impl/CampaignServiceImpl.java
+++ campaign/src/main/java/com/campaign/service/impl/CampaignServiceImpl.java
@@ -4,6 +4,9 @@ import com.campaign.entity.Campaign;
 import com.campaign.exception.EntityNotFoundException;
 import com.campaign.repository.CampaignRepository;
 import com.campaign.service.CampaignService;
+import com.campaign.util.Mapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,21 +17,29 @@ import java.util.Optional;
 @Service
 public class CampaignServiceImpl implements CampaignService {
 
+    Logger logger = LogManager.getLogger(CampaignServiceImpl.class);
+
     @Autowired
     private CampaignRepository campaignRepository;
 
     @Override
     public Campaign createCampaign(Campaign campaign) {
 
+        logger.info("CampaignServiceImpl: createCampaign invoked, request object {}", Mapper.mapToJsonString(campaign));
+
         return campaignRepository.save(campaign);
     }
 
     @Override
     public List<Campaign> getCampaigns() throws EntityNotFoundException{
+
+        logger.info("CampaignServiceImpl: getCampaigns invoked");
        List<Campaign> campaignList = campaignRepository.findAll();
        if(!campaignList.isEmpty()){
+           logger.info("CampaignServiceImpl: getCampaigns reponse object: {}", Mapper.mapToJsonString(campaignList));
            return campaignList;
        }else {
+           logger.error("CampaignServiceImpl: getCampaigns : encountered errors while fecthing data {}", Mapper.mapToJsonString(new EntityNotFoundException("No Campaigns Present !!")));
            throw new EntityNotFoundException("No Campaigns Present !!");
        }
     }
@@ -65,24 +76,30 @@ public class CampaignServiceImpl implements CampaignService {
 
     @Override
     public Campaign getCampaignById(int id) {
-
+        logger.info("CampaignServiceImpl: getCampaignById invoked, id : {}",id);
         Optional<Campaign> campaign = campaignRepository.findById(id);
 
         if (campaign.isPresent()){
+            logger.info("CampaignServiceImpl: getCampaignById reponse object: {}", Mapper.mapToJsonString(campaign.get()));
             return campaign.get();
         }else{
+            logger.error("CampaignServiceImpl: getCampaignById : encountered errors while fecthing data {}", Mapper.mapToJsonString(new EntityNotFoundException("Campaign not found with id: " + id)));
            throw  new EntityNotFoundException("Campaign not found with id: " + id);
         }
     }
 
     @Override
     public String deleteCampaignById(int id) {
+        logger.info("CampaignServiceImpl: deleteCampaignById invoked, id : {}",id);
         Optional<Campaign> campaign = campaignRepository.findById(id);
 
         if (campaign.isPresent()){
+            logger.info("CampaignServiceImpl: deleteCampaignById reponse object: deleted the campaign with id :{} successfully !! ", id);
             campaignRepository.deleteById(id);
             return "deleted the campaign with id : "+id+" successfully !!";
         }else{
+
+            logger.error("CampaignServiceImpl: deleteCampaignById : encountered errors while fecthing data {}", Mapper.mapToJsonString(new EntityNotFoundException("Campaign not found with id: " + id)));
             throw  new EntityNotFoundException("Campaign not found with id: " + id);
         }
     }
