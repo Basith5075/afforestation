8a896a559e16ca58af0c3543162252560a6191ca
Basith5075 <65912815+Basith5075@users.noreply.github.com>
2024-06-25 22:37:38 -0500

    Splunk integration

--- campaign/src/main/java/com/campaign/controller/CampaignController.java
+++ campaign/src/main/java/com/campaign/controller/CampaignController.java
@@ -2,6 +2,9 @@ package com.campaign.controller;
 
 import com.campaign.entity.Campaign;
 import com.campaign.service.CampaignService;
+import com.campaign.util.Mapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,36 +17,41 @@ import java.util.Map;
 @RequestMapping(value = "/v1/campaign")
 public class CampaignController {
 
+    Logger logger = LogManager.getLogger(CampaignController.class);
+
     @Autowired
     private CampaignService campaignService;
 
     @GetMapping("/get")
-    public ResponseEntity<List<Campaign>> getCampaigns(){
-
-        return new ResponseEntity<>(campaignService.getCampaigns(),HttpStatus.OK);
+    public ResponseEntity<List<Campaign>> getCampaigns() {
+        logger.info("CampaignController: getCampaigns invoked: response object {}:", Mapper.mapToJsonString(campaignService.getCampaigns()));
+        return new ResponseEntity<>(campaignService.getCampaigns(), HttpStatus.OK);
     }
 
     @PostMapping("/create")
-    public Campaign createCampaign(@RequestBody Campaign campaign){
+    public Campaign createCampaign(@RequestBody Campaign campaign) {
+
+        logger.info("CampaignController: createCampaign invoked: request object {}", Mapper.mapToJsonString(campaign));
         return campaignService.createCampaign(campaign);
     }
 
     // update
 
     @PatchMapping("/update/{id}")
-    public Campaign updateCampaign(@PathVariable int id, @RequestBody Map<String, Object> campaign){
-        return campaignService.updateCampaignPartially(campaign,id);
+    public Campaign updateCampaign(@PathVariable int id, @RequestBody Map<String, Object> campaign) {
+        return campaignService.updateCampaignPartially(campaign, id);
     }
 
     @GetMapping("/get/{id}")
-    public Campaign getCampaignById(@PathVariable int id){
-
+    public Campaign getCampaignById(@PathVariable int id) {
+        logger.info("CampaignController: getCampaignById invoked: request id {}", id);
         return campaignService.getCampaignById(id);
     }
 
 
     @DeleteMapping("/delete/{id}")
-    public String deleteCampaignById(@PathVariable int id){
+    public String deleteCampaignById(@PathVariable int id) {
+        logger.info("CampaignController: deleteCampaignById invoked: request id {}", id);
         return campaignService.deleteCampaignById(id);
     }
 
--- campaign/src/main/java/com/campaign/hashicorp/AppService.java
+++ campaign/src/main/java/com/campaign/hashicorp/AppService.java
@@ -1,19 +1,19 @@
-package com.campaign.hashicorp;
-
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-@Slf4j
-@Service
-@RequiredArgsConstructor
-public class AppService {
-
-    private final AppConfiguration appConfiguration;
-    @PostConstruct
-    public void readConfigs() {
-        log.info("Reading configuration {} - {} - {}",
-                appConfiguration.getPassword(), appConfiguration.getUsername(), appConfiguration.getUrl());
-    }
-}
+//package com.campaign.hashicorp;
+//
+//import jakarta.annotation.PostConstruct;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
+//
+//@Slf4j
+//@Service
+//@RequiredArgsConstructor
+//public class AppService {
+//
+//    private final AppConfiguration appConfiguration;
+//    @PostConstruct
+//    public void readConfigs() {
+//        log.info("Reading configuration {} - {} - {}",
+//                appConfiguration.getPassword(), appConfiguration.getUsername(), appConfiguration.getUrl());
+//    }
+//}
