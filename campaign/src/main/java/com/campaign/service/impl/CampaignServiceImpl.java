f3584b3b4c2fab6a6504aedc54909f06f4279e3b
Basith5075 <65912815+Basith5075@users.noreply.github.com>
2024-06-27 23:46:34 -0500

    Added event listeners to CampaignService class that will update the Campaign's current amount

--- campaign/src/main/java/com/campaign/service/impl/CampaignServiceImpl.java
+++ campaign/src/main/java/com/campaign/service/impl/CampaignServiceImpl.java
@@ -1,6 +1,7 @@
 package com.campaign.service.impl;
 
 import com.campaign.entity.Campaign;
+import com.campaign.events.CampaignUpdate;
 import com.campaign.exception.EntityNotFoundException;
 import com.campaign.repository.CampaignRepository;
 import com.campaign.service.CampaignService;
@@ -8,6 +9,7 @@ import com.campaign.util.Mapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -31,17 +33,17 @@ public class CampaignServiceImpl implements CampaignService {
     }
 
     @Override
-    public List<Campaign> getCampaigns() throws EntityNotFoundException{
+    public List<Campaign> getCampaigns() throws EntityNotFoundException {
 
         logger.info("CampaignServiceImpl: getCampaigns invoked");
-       List<Campaign> campaignList = campaignRepository.findAll();
-       if(!campaignList.isEmpty()){
-           logger.info("CampaignServiceImpl: getCampaigns reponse object: {}", Mapper.mapToJsonString(campaignList));
-           return campaignList;
-       }else {
-           logger.error("CampaignServiceImpl: getCampaigns : encountered errors while fecthing data {}", Mapper.mapToJsonString(new EntityNotFoundException("No Campaigns Present !!")));
-           throw new EntityNotFoundException("No Campaigns Present !!");
-       }
+        List<Campaign> campaignList = campaignRepository.findAll();
+        if (!campaignList.isEmpty()) {
+            logger.info("CampaignServiceImpl: getCampaigns reponse object: {}", Mapper.mapToJsonString(campaignList));
+            return campaignList;
+        } else {
+            logger.error("CampaignServiceImpl: getCampaigns : encountered errors while fecthing data {}", Mapper.mapToJsonString(new EntityNotFoundException("No Campaigns Present !!")));
+            throw new EntityNotFoundException("No Campaigns Present !!");
+        }
     }
 
 
@@ -62,45 +64,55 @@ public class CampaignServiceImpl implements CampaignService {
                 case "purpose":
                     existingCampaign.setPurpose((String) value);
                 case "goalAmount":
-                    existingCampaign.setGoalAmount(((Double)value).floatValue());
+                    existingCampaign.setGoalAmount(((Float) value));
                     break;
                 case "currentAmount":
-                    existingCampaign.setCurrentAmount(((Double)value).floatValue());
+                    existingCampaign.setCurrentAmount(((Float) value));
+                case "incrementCurrentAmount":
+                    existingCampaign.setCurrentAmount(existingCampaign.getCurrentAmount()+((Float) value));
                     break;
                 // Add cases for other fields as needed...
             }
         });
 
-        return  campaignRepository.save(existingCampaign);
+        return campaignRepository.save(existingCampaign);
     }
 
     @Override
     public Campaign getCampaignById(int id) {
-        logger.info("CampaignServiceImpl: getCampaignById invoked, id : {}",id);
+        logger.info("CampaignServiceImpl: getCampaignById invoked, id : {}", id);
         Optional<Campaign> campaign = campaignRepository.findById(id);
 
-        if (campaign.isPresent()){
+        if (campaign.isPresent()) {
             logger.info("CampaignServiceImpl: getCampaignById reponse object: {}", Mapper.mapToJsonString(campaign.get()));
             return campaign.get();
-        }else{
+        } else {
             logger.error("CampaignServiceImpl: getCampaignById : encountered errors while fecthing data {}", Mapper.mapToJsonString(new EntityNotFoundException("Campaign not found with id: " + id)));
-           throw  new EntityNotFoundException("Campaign not found with id: " + id);
+            throw new EntityNotFoundException("Campaign not found with id: " + id);
         }
     }
 
     @Override
     public String deleteCampaignById(int id) {
-        logger.info("CampaignServiceImpl: deleteCampaignById invoked, id : {}",id);
+        logger.info("CampaignServiceImpl: deleteCampaignById invoked, id : {}", id);
         Optional<Campaign> campaign = campaignRepository.findById(id);
 
-        if (campaign.isPresent()){
+        if (campaign.isPresent()) {
             logger.info("CampaignServiceImpl: deleteCampaignById reponse object: deleted the campaign with id :{} successfully !! ", id);
             campaignRepository.deleteById(id);
-            return "deleted the campaign with id : "+id+" successfully !!";
-        }else{
+            return "deleted the campaign with id : " + id + " successfully !!";
+        } else {
 
             logger.error("CampaignServiceImpl: deleteCampaignById : encountered errors while fecthing data {}", Mapper.mapToJsonString(new EntityNotFoundException("Campaign not found with id: " + id)));
-            throw  new EntityNotFoundException("Campaign not found with id: " + id);
+            throw new EntityNotFoundException("Campaign not found with id: " + id);
         }
     }
+
+    //  Created a listener in campaignService to listen events of update in the currentAmount and update the currentAmount accordingly.
+    @EventListener
+    private void updateCurrentGoalAmount(CampaignUpdate campaignUpdate) {
+
+        updateCampaignPartially(Map.of("incrementCurrentAmount", campaignUpdate.getAmount()), campaignUpdate.getCampaignId());
+    }
+
 }
